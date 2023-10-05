0f756a91a1dbbcb05ee13f6c1da05622b089a79a
Ilya Delidon <illadelidon95@gmail.com>
2023-10-05 15:10:14 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import SignIn from "./pages/auth/signin";
+import { useTypedSelector } from "./hooks/useTypedSelector";
+import NotFound from "./pages/notFound";
+import DashboardLayout from "./container/dashboardLayout";
+import DefaultPage from "./pages/dafaultPage";
+import AllUsers from "./pages/users/allUsers";
+
+function App() {
+  const { isAuth, user } = useTypedSelector((store) => store.UserReducer);
+
+  return (
+    <Routes>
+      {isAuth && (
+        <>
+          {user.role === "Administrator" && (
+            <Route path="/dashboard" element={<DashboardLayout />}>
+              <Route index element={<DefaultPage />} />
+              <Route path="users" element={<AllUsers />} />
+            </Route>
+          )}
+          {user.role === "User" && (
+            <Route path="/dashboard" element={<DashboardLayout />}>
+              <Route path="users" element={<AllUsers />} />
+            </Route>
+          )}
+        </>
+      )}
+
+      <Route path="/" element={<SignIn />} />
+      <Route path="/dashboard" element={<SignIn />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default App;
